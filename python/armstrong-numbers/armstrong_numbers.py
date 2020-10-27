cd4fcc007e0b023f7965091ef9820718e9f73b3c
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2020-10-27 10:32:42 -0700

    Add files via upload

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,1 +1,9 @@
-
+def is_armstrong_number(number):
+    numList = [int(x) for x in str(number)]
+    total = 0
+    for n in numList:
+        total += n ** len(numList)
+    if total == number:
+        return True
+    else:
+        return False
