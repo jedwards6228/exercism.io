9ec94295355bef6616bc0b64681a882d7ac87b4a
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2020-10-27 10:26:19 -0700

    Add files via upload

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,1 +1,13 @@
-
+def is_pangram(sentence):
+    #make sentence lowercase and part of a set
+    mySet = {str(x) for x in sentence.lower()}
+    #remove non-alphanumeric characters and spaces
+    for x in sentence:
+        if x.isalpha() == False:
+            mySet.discard(x)
+    #check length against the target length
+    if len(mySet) == 26:
+        return True
+    else:
+        return False
+    pass
