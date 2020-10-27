cfc09a94bb1ba83e092b70bda9b0085f3db94156
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2020-10-27 10:03:15 -0700

    Add files via upload

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,1 +1,12 @@
+class Matrix:
+    def __init__(self, matrix_string):
+        #create a list with lists inside of it, each imbedded list is a row.
+        self.matrix_rows = [[int(num) for num in line.split()] for line in matrix_string.split("\n")]
 
+    def row(self, index):
+        #return the row that is asked for.
+        return self.matrix_rows[index - 1]
+
+    def column(self, index):
+        #return a list that is made up of a single index from each row.
+        return [col[index - 1] for col in self.matrix_rows]
