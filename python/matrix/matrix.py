909f948817a9af539db636b47a8fd9548773cafe
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2020-10-29 14:49:40 -0700

    Update matrix.py

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,6 +1,6 @@
 class Matrix:
     def __init__(self, matrix_string):
-        #create a list with lists inside of it, each imbedded list is a row.
+        #create a list with lists inside of it, each embedded list is a row.
         self.matrix_rows = [[int(num) for num in line.split()] for line in matrix_string.split("\n")]
 
     def row(self, index):
