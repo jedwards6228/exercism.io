e3ad16ea8a00c40b49b825f55c31bea1dc0ab234
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2020-10-28 17:29:01 -0700

    Changes from work
    self.used_names needs to be different somehow.

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,3 +1,27 @@
+import random
+
+
 class Robot:
     def __init__(self):
-        pass
+        self.name = self.robot_name()
+        self.used_names = []
+
+    def robot_name(self):
+        name = ""
+        while len(name) < 5:
+            while len(name) < 2:
+                name = self.add_let(name)
+            name = self.add_num(name)
+            if len(name) == 5:
+                if name in self.used_names:
+                    name = ""
+        self.used_names.append(name)
+        return name
+
+    def add_let(self, name):
+        name += random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        return name
+
+    def add_num(self, name):
+        name += str(random.randint(0, 9))
+        return name
