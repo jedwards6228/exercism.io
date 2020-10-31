56e2750f9a0dcda974a5a82dcc3ce2c404702577
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2020-10-30 17:30:53 -0700

    Update robot_name.py
    Made good progress at work

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -2,20 +2,24 @@ import random
 
 
 class Robot:
-
-    used_name = []
+    used_name = set()
     name = ''
 
     def __init__(self):
-        self.
+        self.reset()
 
     def robot_name(self):
         while len(self.name) < 5:
             while len(self.name) < 2:
                 self.name += random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
             self.name += str(random.randint(0, 9))
-            if len(self.name) == 5:
+            if len(self.name) == 5 and len(self.used_name) > 0:
                 if self.name in self.used_name:
                     self.name = ''
-        self.used_name.append(self.name)
+        self.used_name.add(self.name)
         return self.name
+
+    def reset(self):
+        self.name = ''
+        self.robot_name()
+        return
